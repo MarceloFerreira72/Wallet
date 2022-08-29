65e3d654f46b1bbbd7e7b692cca25efb0433ef2c
Marcelo Ferreira <marceloferreira.ihu72@gmail.com>
2022-08-29 15:42:05 -0300

    requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,69 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login as loginAction } from '../redux/actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  validateInputs = () => {
+    const { email, password } = this.state;
+    const NUMBER_SIX = 6;
+    const atSymbol = email.indexOf('@');
+    const dot = email.indexOf('.', atSymbol);
+    const validateEmail = (
+      atSymbol > 1 && dot >= atSymbol + 2 && dot !== email.length - 1
+    );
+    const validatePass = password.length >= NUMBER_SIX;
+    return !(validateEmail && validatePass);
+  };
+
+  handleLogin = (e) => {
+    e.preventDefault();
+    const { dispatch, history } = this.props;
+    const { email } = this.state;
+
+    dispatch(loginAction({ email }));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    return (
+      <form onSubmit={ this.handleLogin }>
+        <input
+          type="email"
+          onChange={ (e) => this.setState({ email: e.target.value }) }
+          placeholder="E-mail"
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          onChange={ (e) => this.setState({ password: e.target.value }) }
+          placeholder="Senha"
+          data-testid="password-input"
+        />
+        <button
+          type="submit"
+          disabled={ this.validateInputs() }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect()(Login);
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,17 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+// import actions from ../actions
+
+const initialState = {
+  currencies: [],
+  expenses: [],
+  editor: false,
+  idToEdit: 0,
+};
+
+function walletReducer(state = initialState, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
+
+export default walletReducer;
